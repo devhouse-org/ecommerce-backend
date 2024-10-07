90a10969f8ca37ce51f112816c12110145831487
Ahmed Ghazi <ahmed.ghazi@iqdevhouse.com>
2024-10-07 12:28:23 +0300

    add fav model and seed

---
+++ prisma/migrations/20241007092742_fav/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "FavoriteProduct" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "FavoriteProduct_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "FavoriteProduct" ADD CONSTRAINT "FavoriteProduct_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FavoriteProduct" ADD CONSTRAINT "FavoriteProduct_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,102 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly prisma: PrismaService) { }
 
+  // seed end point
+  @Get('seed/users')
+  async seed() {
+    const dummyUsers = Array.from({ length: 20 }, (_, index) => ({
+      email: `user${index + 1}@example.com`,
+      password: 'password123', // Use a hashed password in production
+      name: `User ${index + 1}`,
+    }));
+
+    await this.prisma.user.createMany({
+      data: dummyUsers,
+      skipDuplicates: true, // In case users with the same email already exist
+    });
+    return 'Seeding done';
+  }
+  // another seed endpoint
+  @Get('seed/categories')
+  async seedCategories() {
+    // Create 10 dummy categories
+    const dummyCategories = Array.from({ length: 10 }, (_, index) => ({
+      name: `Category ${index + 1}`,
+    }));
+
+    const categories = await this.prisma.category.createMany({
+      data: dummyCategories,
+      skipDuplicates: true, // In case categories with the same name already exist
+    });
+
+    console.log(`${dummyCategories.length} categories created!`);
+
+    // Create 50 dummy products
+    const dummyProducts = Array.from({ length: 50 }, (_, index) => ({
+      name: `Product ${index + 1}`,
+      description: `Description for Product ${index + 1}`,
+      price: Math.random() * 100, // Random price between 0 and 100
+      imageUrl: `https://via.placeholder.com/150?text=Product+${index + 1}`,
+
+    }));
+
+    await this.prisma.product.createMany({
+      data: dummyProducts,
+      skipDuplicates: true,
+    });
+    return `${dummyProducts.length} products created!`;
+  }
+
+  // another seed endpoint
+  @Get('seed/connect-categories-to-products')
+  async seedOrders() {
+    try {
+      // Fetch all products and categories
+      const products = await this.prisma.product.findMany();
+      const categories = await this.prisma.category.findMany();
+
+      if (products.length === 0 || categories.length === 0) {
+        throw new HttpException(
+          'No products or categories available',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+
+      // Randomly connect products to categories
+      for (const product of products) {
+        // Get a random number of categories to assign to the product
+        const randomCategoryCount = Math.floor(Math.random() * categories.length) + 1;
+        // Shuffle categories and take a random number of them
+        const randomCategories = this.shuffleArray(categories).slice(0, randomCategoryCount);
+
+        // Connect the product with the selected random categories
+        await this.prisma.product.update({
+          where: { id: product.id },
+          data: {
+            category: {
+              connect: randomCategories.map((category) => ({ id: category.id })),
+            },
+          },
+        });
+      }
+
+      return { message: 'Random categories assigned to products successfully' };
+    } catch (error) {
+      throw new HttpException('Failed to connect categories', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+  shuffleArray(array: any[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
   @Get()
   getHello(): string {
     return this.appService.getHello();
